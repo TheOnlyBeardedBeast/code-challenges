e94e68eadd22c44324c038aa06daab2ccde006db
Gorgan Daniel <daniel.gorgan.ext@fashion-digital.de>
2024-06-26 10:01:33 +0300

    update collation sql example

--- duplicates-detection/sql/collation.sql
+++ duplicates-detection/sql/collation.sql
@@ -1,5 +1,8 @@
+-- CREATE COLLATION german (provider = icu, locale = 'de');
+SELECT 'ü' = 'u' COLLATE german; -- false
+
 CREATE COLLATION ignore_accent_case (provider = icu, deterministic = false, locale = 'de_DE-u-ks-level1');
 
-SELECT 'Å' = 'A' COLLATE ignore_accent_case; -- true
+SELECT 'Å' = 'A' COLLATE ignore_accent_case_2; -- true
 SELECT 'z' = 'Z' COLLATE ignore_accent_case; -- true
-SELECT 'ü' = 'ue' COLLATE ignore_accent_case; -- false
+SELECT 'ü' = 'ue' COLLATE ignore_accent_case; -- true
